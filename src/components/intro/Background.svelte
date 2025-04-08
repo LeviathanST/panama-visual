7a621459d2b65a2abea44176996f2e3303d292de
LeviathanST <phamquochung170505@gmail.com>
2025-04-09 01:45:01 +0700

    add(component/bg): autoplay from ytb-embed

---
+++ src/components/intro/Background.svelte
@@ -0,0 +1,23 @@
+<div class="bg h-full w-full absolute">
+    <img
+        src="/images/header_bg_banner_mask.png"
+        alt="bg-mask"
+        class="absolute w-full h-full z-10"
+    />
+    <div class="bg-inner w-full h-full relative">
+        <iframe
+            id="YTPlayer-ID-1744134110124"
+            class="ytplayer-player"
+            style="width: 100%; height: 100%; left: 0px; top: -237px;"
+            frameborder="0"
+            allowfullscreen
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            title="banner"
+            src="https://www.youtube.com/embed/lG-fqfLrZms?iv_load_policy=3&amp;modestbranding=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0&amp;wmode=opaque&amp;branding=0&amp;autohide=0&amp;rel=0&amp;start=0&mute=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Feclipspictures.com&amp;widgetid=1&amp;forigin=https%3A%2F%2Feclipspictures.com%2F&amp;aoriginsup=0&amp;gporigin=https%3A%2F%2Feclipspictures.com%2Fportfolio&amp;vf=1"
+        ></iframe>
+    </div>
+</div>
+
+<style>
+</style>
