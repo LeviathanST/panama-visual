98aa67bd1f43e630af978f3c65320cd536ae90eb
LeviathanST <phamquochung170505@gmail.com>
2025-04-10 17:22:17 +0700

    add(banner): bg, menu_outer

---
+++ src/components/intro/Banner.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Background from "./Background.svelte";
+    import MenuOuter from "./MenuOuter.svelte";
+</script>
+
+<header id="header" class="banner h-[60vw] w-full relative">
+    <Background />
+    <MenuOuter />
+</header>
